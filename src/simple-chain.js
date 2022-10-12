245cc34b1b8eccc72b64efacd286c2e86ace242b
Bonus156 <Bonus.by@mail.ru>
2022-10-12 22:30:23 +0300

    feat: do simple-chain, fix what-season

--- src/simple-chain.js
+++ src/simple-chain.js
@@ -5,27 +5,35 @@ const { NotImplementedError } = require('../extensions/index.js');
  * 
  */
 const chainMaker = {
+  chain: [],
   getLength() {
-    throw new NotImplementedError('Not implemented');
-    // remove line with error and write your code here
+    return this.chain.length;    
   },
-  addLink(/* value */) {
-    throw new NotImplementedError('Not implemented');
-    // remove line with error and write your code here
+  addLink(value) {
+    this.chain.push(`( ${value} )`);
+    return this;
   },
-  removeLink(/* position */) {
-    throw new NotImplementedError('Not implemented');
-    // remove line with error and write your code here
+  removeLink(position) {
+    if (typeof position === 'number' && position < this.chain.length && position > 0 && position === Math.round(position)) {
+      this.chain.splice(position - 1, 1);
+      return this;
+    } 
+    this.chain = [];
+    throw new Error('You can\'t remove incorrect link!');
   },
   reverseChain() {
-    throw new NotImplementedError('Not implemented');
-    // remove line with error and write your code here
+    this.chain.reverse();
+    return this;
   },
   finishChain() {
-    throw new NotImplementedError('Not implemented');
-    // remove line with error and write your code here
+    const answer = this.chain.join('~~');
+    this.chain = [];
+    return answer;
   }
 };
+
+// chainMaker.addLink(function () { }).addLink('2nd').addLink('3rd').removeLink(2).reverseChain().finishChain();
+// console.log(chainMaker.addLink(function () { }).addLink('2nd').addLink('3rd').removeLink(2).reverseChain().finishChain());
 
 module.exports = {
   chainMaker
--- src/what-season.js
+++ src/what-season.js
@@ -12,25 +12,21 @@ const { NotImplementedError } = require('../extensions/index.js');
  * 
  */
 function getSeason(date) {
-  if (!date) {
-    throw new Error('Unable to determine the time of year!');
-  }
+  if (!date) return 'Unable to determine the time of year!';
+  
   if (!(date instanceof Date)) throw new Error('Invalid date!');
   const arr = Object.keys(date);
   if(arr.length > 0) {
     throw new Error('Invalid date!')
   }
-  // if (!(date)) throw new Error('Unable to determine the time of year!');
-  let month = date.getMonth();
-  // console.log(month);
+  // if (!(date.hasOwnProperty.length)) throw new Error('Unable to determine the time of year!');
+  let month = date.getMonth();  
   if (month < 2 || month === 11) return 'winter';
   if (month < 5) return 'spring';
   if (month < 8) return 'summer';
   if (month < 11) return 'autumn';
   else throw new Error('Unable to determine the time of year!');
 }
-
-// console.log(getSeason());
 
 module.exports = {
   getSeason
